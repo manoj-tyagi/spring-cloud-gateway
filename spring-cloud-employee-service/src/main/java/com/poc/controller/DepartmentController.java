5ac5531d79150aa6de04ddeac5d65b3740e4f996
Manoj Tyagi <manoj.imr@gmail.com>
2018-07-07 22:03:06 +0530

    uses spring profile in logback xml

--- spring-cloud-employee-service/src/main/java/com/poc/controller/DepartmentController.java
+++ spring-cloud-employee-service/src/main/java/com/poc/controller/DepartmentController.java
@@ -1,8 +1,11 @@
 package com.poc.controller;
 
+import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,10 +16,12 @@ import com.poc.model.Department;
 
 @RestController
 public class DepartmentController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 	
 	@GetMapping("/departments")
 	public List<Department> findAll(@RequestHeader("userId") String userId) {
-		System.out.println("userId================="+userId);
+		LOGGER.info("findAll() department...");
+		LOGGER.info("userId================={}", userId);
 		List<Department> departments = new ArrayList<>();
 		Department dep = new Department();
 		dep.setId(1L);
@@ -30,7 +35,7 @@ public class DepartmentController {
 	
 	@GetMapping("/departments/departmentId/{departmentId}")
 	public ResponseEntity<Department> findById(@PathVariable String departmentId, @RequestHeader("userId") String userId) {
-		System.out.println("userId================="+userId);
+		LOGGER.info("userId================={}", userId);
 		Department dep = new Department();
 		dep.setId(Long.valueOf(departmentId));
 		dep.setName("SALES");
